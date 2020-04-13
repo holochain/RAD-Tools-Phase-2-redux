bba5fc873e4ff56709dd39257544b7b206e62d4a
Robbie Carlton <robbie.carlton@gmail.com>
2020-04-13 12:58:32 -0600

    formatting

--- ui/scripts/generate-schema.js
+++ ui/scripts/generate-schema.js
@@ -6,11 +6,9 @@ const typeSpec = JSON.parse(fs.readFileSync(typeSpecUrl))
 fs.writeFileSync('schema.gql', renderSchema(typeSpec))
 
 function renderSchema ({ types }) {
-  return `
-    ${mapObject(types, renderTypeDef).join('\n')}
-    ${renderQueryDef(types)}
-    ${renderMutationDef(types)}
-  `
+  return `${mapObject(types, renderTypeDef).join('\n')}
+${renderQueryDef(types)}
+${renderMutationDef(types)}`
 }
 
 function mapObject (object, fn) {
@@ -18,17 +16,16 @@ function mapObject (object, fn) {
 }
 
 function renderTypeDef (typeName, type) {
-  return `
-    type ${typeName} {
-      id: ID
+  return `type ${typeName} {
+  id: ID
 ${mapObject(type, renderField).join('\n')}
-    }
+}
 
-    input ${typeName}Input {
-      id: ID
+input ${typeName}Input {
+  id: ID
 ${mapObject(type, renderField).join('\n')}      
-    }
-  `
+}
+`
 }
 
 function renderField (name, type) {
@@ -36,34 +33,30 @@ function renderField (name, type) {
     string: 'String'
   }
   const graphqlType = rustToGraphqlType[type]
-  return `      ${name}: ${graphqlType}`
+  return `  ${name}: ${graphqlType}`
 }
 
 function renderQueryDef (types) {
   return `Query {
-      ${mapObject(types, renderTypeQueries).join('')}
-    }
-  `
+${mapObject(types, renderTypeQueries).join('')}}
+`
 }
 
 function renderTypeQueries (typeName) {
-  return `
-      get${typeName}(id: ID): ${typeName}
-      list${typeName}s: [${typeName}]
-  `
+  return `  get${typeName}(id: ID): ${typeName}
+  list${typeName}s: [${typeName}]
+`
 }
 
 function renderMutationDef (types) {
   return `Mutation {
-      ${mapObject(types, renderTypeMutations).join('')}
-    }
-  `
+${mapObject(types, renderTypeMutations).join('')}}
+`
 }
 
 function renderTypeMutations (typeName) {
-  return `
-      create${typeName}(input: ${typeName}Input): ${typeName}
-      update${typeName}(input: ${typeName}Input): ${typeName}
-      delete${typeName}(id: ID): ${typeName}
-    `
+  return `  create${typeName}(input: ${typeName}Input): ${typeName}
+  update${typeName}(input: ${typeName}Input): ${typeName}
+  delete${typeName}(id: ID): ${typeName}
+`
 }
