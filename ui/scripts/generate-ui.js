c9bb09537aaced09c8672ac37e92b62c1414e881
Robbie Carlton <robbie.carlton@gmail.com>
2020-04-14 13:54:19 -0600

    generate pages

--- ui/scripts/generate-ui.js
+++ ui/scripts/generate-ui.js
@@ -3,13 +3,17 @@ const fs = require('fs')
 const renderSchema = require('./renderSchema')
 const renderResolvers = require('./renderResolvers')
 const renderGeneratedHapp = require('./renderGeneratedHapp')
+const renderTypePage = require('./renderTypePage')
 const ncp = require('ncp')
+const mapObject = require('./render-utils').mapObject
 
-const SOURCE_PATH = './ui/ui_scaffold'
+const SOURCE_PATH = './ui/ui_template'
 const DESTINATION_PATH = './ui/generated_ui'
+
 const SCHEMA_PATH = './ui/generated_ui/src/schema.js'
 const RESOLVERS_PATH = './ui/generated_ui/src/resolvers.js'
 const GENERATED_HAPP_PATH = './ui/generated_ui/src/GeneratedHapp.js'
+const PAGES_PATH = './ui/generated_ui/src/pages/'
 
 const renderers = [
   [renderSchema, SCHEMA_PATH],
@@ -27,4 +31,11 @@ ncp(SOURCE_PATH, DESTINATION_PATH, err => {
 
   renderers.forEach(([renderFunction, path]) =>
     fs.writeFileSync(path, renderFunction(typeSpec)))
+
+  mapObject(typeSpec.types, generateTypePage)
 })
+
+function generateTypePage (typeName, type) {
+  const path = PAGES_PATH + typeName + 'sPage.js'
+  fs.writeFileSync(path, renderTypePage(typeName, type))
+}
