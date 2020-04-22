468b5c0fa6c83ead70302e4efeb97dea311a0d34
jetttech <lisa.jetton@holo.host>
2020-04-21 21:40:56 -0500

    add agent to config template

--- setup/agent-setup/add-agent-to-conductor.js
+++ setup/agent-setup/add-agent-to-conductor.js
@@ -1,18 +1,52 @@
-const { exec } = require('child_process');
+const fs = require('fs')
+const util = require('util')
+const toml = require('toml')
+const concat = require('concat-stream')
+const exec = util.promisify(require('child_process').exec);
+ 
+const conductorConfig = require('../../conductor-config.example.toml')
+// const conductorConfig = require('../../conductor-config.toml')
+const streamConfig = () => fs.createReadStream(conductorConfig, 'utf8').pipe(concat(data => {
+  console.log('>>>>>>>>>>>> parsed toml: \n', toml.parse(data))
+  return toml.parse(data)
+}))
 
-let agentKeys = []
+const agentName = process.argv[2]
+const pathToConfig = '../../conductor-config/example.toml'
 
-exec("find ./keystores -name *.keystore  | sed -ne 's/\(?<=_\)\(.*\)\(?=.keystore\)$/\2/p'", (err, stdout, stderr) => {
-  if (err) return console.error(err)
-  else {
-    if (stderr) console.log(`stderr: ${stderr}`)
-    console.log('stdout : ', stdout)
+const generateAgentConfig = (agentName, agentPubKey) => `
+  id = "${agentName}"
+  keystore_file = "keystores/${agentName}/${agentPubKey}>.keystore"
+  name = "${agentName}"
+  public_address = "${agentPubKey}"
+`
 
-    const agentKeys = new RegExp('(?<=_)(.*)(?=.keystore)').exec(stdout)
-    console.log(agentKeys);
-    
-  //  const agentKeystores = Array.of(stdout)
-  //  agentKeys =agentKeystores.map(agentKeystore => agentKeystore.split('.')[0])
-  //  console.log('agentKeys : ', agentKeys)
+if(agentName) {
+  locateAgentPubKey()
+    .then(agentPubKey => addAgentToConfig(generateAgentConfig(agentName, agentPubKey), pathToConfig))
+}
+else {
+  console.log(`Error: Agent Name required as argument.`)
+} 
+
+async function locateAgentPubKey() {
+  try {
+      const { stderr } = await exec(`find ./keystores/${agentName} -name *.keystore  | sed -ne 's/^(.*)(?=.keystore)//p'`)
+      if (stderr) console.log('stderr:', stderr)
+  } catch (err) {
+     console.error(err)
+
+     if (stderr) console.log(`stderr: ${stderr}`)
+     console.log('stdout : ', stdout)
+ 
+     // const agentKeys = new RegExp('(?<=_)(.*)(?=.keystore)').exec(stdout)
+     // console.log(agentKeys)
+     const agentPubKey = stdout.trim()
+     return agentPubKey
+     // add agentConfigTemplate to 
   }
+}
+
+const addAgentToConfig = (agentConfigTemplate, pathToConfig) => fs.writeFileSync(pathToConfig, () => {
+  streamConfig()
 })
--- setup/agent-setup/create-new-agent.js
+++ setup/agent-setup/create-new-agent.js
@@ -1,7 +1,7 @@
 const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const exec = util.promisify(require('child_process').exec)
 
-const agentName = process.argv[2] || "agent"
+const agentName = process.argv[2] || "agent-1"
 
 async function checkIfKeystoresDirExists() {
   try {
@@ -26,10 +26,11 @@ async function createNewAgentKeystore() {
       const { stdout, stderr } = await exec(`hc keygen -n --path ./keystores/${agentName}/AGENT_1_PUB_KEY.keystore | sed -ne 's/^Public address: //p'`)
       if (stderr) console.log('stderr:', stderr)
       let agentPubKey = stdout.trim()
-      console.log("Succesfully created new agent keystore. \n")
-      console.log("Created Public Address: ", agentPubKey)
-      console.log("Keystore written to: ", `./keystores/${agentName}/${agentPubKey}.keystore`)
+      console.log(`Succesfully created new agent keystore. \n`)
+      console.log(`Created Public Address: ${agentPubKey}  \n`)
+      const FILE_RENAME_SUCCESS_MESSAGE = `Keystore written to: ./keystores/${agentName}/${agentPubKey}.keystore. \n`
       await exec(`mv ./keystores/${agentName}/AGENT_1_PUB_KEY.keystore ./keystores/${agentName}/${agentPubKey}.keystore`)
+        .then(_ => console.log(FILE_RENAME_SUCCESS_MESSAGE))
   } catch (err) {
      console.error(err)
   }
