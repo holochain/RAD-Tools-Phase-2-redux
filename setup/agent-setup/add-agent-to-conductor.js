c940890a9caba407bc659b741785f1bc837c4bed
jetttech <lisa.jetton@holo.host>
2020-04-21 19:01:19 -0500

    agent-keygen scripts

--- setup/agent-setup/add-agent-to-conductor.js
+++ setup/agent-setup/add-agent-to-conductor.js
@@ -1,14 +1,18 @@
 const { exec } = require('child_process');
 
-const agentKeys = []
+let agentKeys = []
 
-exec("find ./keystores -name *.keystore  | sed -ne 's/_\.*//p' | xargs -I {} echo '{}'", (err, stdout, stderr) => {
+exec("find ./keystores -name *.keystore  | sed -ne 's/\(?<=_\)\(.*\)\(?=.keystore\)$/\2/p'", (err, stdout, stderr) => {
   if (err) return console.error(err)
   else {
-   if (stderr) console.log(`stderr: ${stderr}`)
-   console.log(`stdout: ${stdout}`)
+    if (stderr) console.log(`stderr: ${stderr}`)
+    console.log('stdout : ', stdout)
 
-   // agentKeys.push(stdout)
-   // agentKeys = stdout
+    const agentKeys = new RegExp('(?<=_)(.*)(?=.keystore)').exec(stdout)
+    console.log(agentKeys);
+    
+  //  const agentKeystores = Array.of(stdout)
+  //  agentKeys =agentKeystores.map(agentKeystore => agentKeystore.split('.')[0])
+  //  console.log('agentKeys : ', agentKeys)
   }
 })
--- setup/agent-setup/create-new-agent.js
+++ setup/agent-setup/create-new-agent.js
@@ -1,26 +1,45 @@
-const { exec } = require('child_process')
-const prompts = require('prompts')
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-let agentName 
+const agentName = process.argv[2]
 
-// prompt user for name of agent
-(async () => {
-  const response = await prompts({
-    type: 'text',
-    name: 'agentName',
-    message: 'What is the name of this Agent?'
-  });
- 
-  console.log(response.agentName)
-  agentName = response.agentName
-})();
-
-
-// create keys, referencing agent name output from prompt as input var for keygen file-name/path:
-exec(`hc keygen -n --path ./keystores/${agentName}_<AGENT_PUB_KEY>.keystore  | sed -ne 's/^Public address:\.*//p' | xargs -I {} mv ./keystores/${agentName}_AGENT_1_PUB_KEY.keystore ./keystores/${agentName}_{}.keystore`, (err, stdout, stderr) => {
-  if (err) return console.error(err)
-  else {
-   if (stderr) console.log(`stderr: ${stderr}`)
-   console.log(`Agent Keys - stdout : ${stdout}`)
+async function checkIfKeystoresDirExists() {
+  try {
+      const { stderr } = await exec(`[ ! -d 'keystores' ] && mkdir keystores; [ -d 'keystores' ]`)
+      if (stderr) console.log('stderr:', stderr)
+      } catch (err) {
+     console.error(err)
   }
-})
+}
+
+async function checkIfAgentDirExists() {
+  try {
+      const { stderr } = await exec(`[ ! -d 'keystores/${agentName}' ] && mkdir keystores/${agentName}; [ -d 'keystores/${agentName}' ]`)
+      if (stderr) console.log('stderr:', stderr)
+  } catch (err) {
+     console.error(err)
+  }
+}
+
+async function createNewAgentKeystore() {
+  try {
+      const { stdout, stderr } = await exec(`sh -c 'hc keygen -n --path ./keystores/${agentName}/AGENT_1_PUB_KEY.keystore | sed -ne 's/^Public address:|.*//p/''`)
+      if (stderr) console.log('stderr:', stderr)
+
+      console.log(stdout)
+ 
+      // const findAgentPubKey = new RegExp(`sed -ne 's/^Public \.*//p'`)
+      // const agentPubKey = findAgentPubKey.exec(stdout)
+      // console.log('agentPubKey :', agentPubKey);
+      
+      // new RegExp(`mv ./keystores/${agentName}/<AGENT_PUB_KEY>.keystore ./keystores/${agentName}/${agentPubKey}.keystore`)
+      
+      //  | sed -ne 's/^Public address:\.*//p' | xargs -I {} mv ./keystores/${agentName}/agentkey.keystore ./keystores/${agentName}/{}.keystore
+  } catch (err) {
+     console.error(err)
+  }
+}
+
+checkIfKeystoresDirExists()
+  .then(_ => checkIfAgentDirExists())
+  .then(_ => createNewAgentKeystore())
