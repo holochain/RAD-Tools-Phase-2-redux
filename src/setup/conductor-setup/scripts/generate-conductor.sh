0b23bab8919b6e20f3b58c2686735295cf95e295
jetttech <lisa.jetton@holo.host>
2020-05-13 00:10:43 -0500

    maintain  agent 1 keypair file

--- src/setup/conductor-setup/scripts/generate-conductor.sh
+++ src/setup/conductor-setup/scripts/generate-conductor.sh
@@ -4,7 +4,7 @@
 cp ./conductor-config.example.toml ./conductor-config.toml
 [ ! -d ./keystores ] && mkdir keystores
 [ ! -d ./keystores/agent1 ] && mkdir keystores/agent1
-[ -f ./keystores/agent1/AGENT_1_PUB_KEY.keystore ] && rm -rf ./keystores/agent1/AGENT_1_PUB_KEY.keystore
+rm -rf ./keystores/agent1/*
 hc keygen -n --path ./keystores/agent1/AGENT_1_PUB_KEY.keystore | sed -ne 's/^Public address:\.*//p' | xargs -I {} sh -c 'mv ./keystores/agent1/AGENT_1_PUB_KEY.keystore ./keystores/agent1/{}.keystore; sed -i "s/<AGENT_1_PUB_KEY>/{}/" ./conductor-config.toml'
 [ ! -L ./ui-src/src/conductor-config.toml ] && ln -s ./conductor-config.toml ./ui-src/src/conductor-config.toml
 echo "Generated new keypair for Agent 1"
