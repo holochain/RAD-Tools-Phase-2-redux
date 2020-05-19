8c3e8942ec9b43c066159ec55a3c80c147339815
jetttech <lisa.jetton@holo.host>
2020-05-19 10:03:46 -0500

    update index for testing

--- src/ui-setup/scripts/renderIndex.js
+++ src/ui-setup/scripts/renderIndex.js
@@ -11,7 +11,7 @@ import HomePage from './HomePage'
 ${mapObject(types, renderPageImport).join('\n')}
 import * as serviceWorker from './serviceWorker'
 
-function HApp () {
+export function HApp () {
   return <ApolloProvider client={apolloClient}>
     <BrowserRouter>
       <Switch>
@@ -22,18 +22,21 @@ ${mapObject(types, renderRoute).join('\n')}
   </ApolloProvider>
 }
 
-ReactDOM.render(<HApp />, document.getElementById('root'))
+const rootElement = document.getElementById('root')
+if (rootElement) {
+  ReactDOM.render(<HApp />, rootElement)
+  ReactModal.setAppElement('#root')
+}
 
 serviceWorker.unregister()
 `
 }
 
 function renderPageImport (typeName) {
-  return `import ${typeName}sPage from './pages/${typeName}Page'`
+  return `import ${typeName}Page from './pages/${typeName}Page'`
 }
 
 function renderRoute (typeName) {
   return `        <Route path='/${typeName.toLowerCase()}' exact component={${typeName}Page} />`
 }
 
-module.exports = renderIndex
