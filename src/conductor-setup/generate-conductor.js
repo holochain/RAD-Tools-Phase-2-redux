e758b4dbb34cc92279912a7de5cf73ade52cab01
jetttech <lisa.jetton@holo.host>
2020-06-03 13:29:55 -0500

    return hash from promise

--- src/conductor-setup/generate-conductor.js
+++ src/conductor-setup/generate-conductor.js
@@ -27,7 +27,8 @@ async function packageDNA () {
   if (dnaPackage.test(stdout)) {
     dnaHash = stdout.trim().replace(dnaPackage, 'DNA_HASH').split('DNA_HASH')[1]
     console.log(chalk.cyan('Completed DNA build with hc package'))
-    return console.log(chalk.cyan('DNA Hash: ' + dnaHash))
+    console.log(chalk.cyan('DNA Hash: ' + dnaHash))
+    return dnaHash
   } else {
     throw new Error('hc package error: Unable to locate compiled DNA hash.')
   }
@@ -59,7 +60,7 @@ async function updateUiDotEnv (instanceId, wsPort) {
 
 generateConductor()
   .then(() => packageDNA())
-  .then(() => updateConductorWithPackagedDna())
+  .then(dnaHash => updateConductorWithPackagedDna(dnaHash))
   .catch(e => {
     if (e.stderr === '/bin/sh: 1: hc: not found\n') {
       console.error(`  ${chalk.red(e.stderr)} \n${chalk.yellow('> Hint: You are probably not running this command in nix-shell. \n  Be sure to enter into nix-shell prior to running any Holochain command.')}`)
